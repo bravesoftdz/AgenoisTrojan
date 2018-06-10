3c71c0339e9f705acf17fd81af1e3b023e042075
ElektroKill <skullmaster0103@gmail.com>
2018-06-10 12:42:01 +0200

    Added Solution

---
+++ Agenois/Start.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace Agenois
+{
+    class Start
+    {
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern bool SetProcessDPIAware();
+        [STAThread]
+        static void Main(string[] args)
+        {
+            SetProcessDPIAware();
+        }
+    }
+}
