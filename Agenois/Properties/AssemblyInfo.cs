cce4bffee5b4650d7cb7506bce92c8760479e83b
ElektroKill <skullmaster0103@gmail.com>
2018-06-10 12:43:49 +0200

    small change

--- Agenois/Properties/AssemblyInfo.cs
+++ Agenois/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Agenois")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Agenois")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Agenois")]
 [assembly: AssemblyProduct("Agenois")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
@@ -17,7 +17,7 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: ComVisible(true)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("22b71dfc-cd8d-4186-b71a-0834c81fec89")]
