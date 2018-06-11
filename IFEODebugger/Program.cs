b0462ebbe4bf19b005e06d22ccc1ef870f3b58b4
ElektroKill <skullmaster0103@gmail.com>
2018-06-11 17:44:51 +0200

    Modified IFEODebugger

--- IFEODebugger/Program.cs
+++ IFEODebugger/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace IFEODebugger
 {
@@ -10,7 +11,8 @@ namespace IFEODebugger
     {
         static void Main(string[] args)
         {
-
+            MessageBox.Show("Your System Was Corrupted By Agenois", ":)", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            Environment.Exit(0);
         }
     }
 }
